679ecae660a27bf4519494cc99e555f65f60a489
David Webb <davweb@users.noreply.github.com>
2017-01-01 09:36:39 +0000

    1. Add Monthly chart 2. Turn off timezones

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,6 +6,14 @@ from .models import Measurement, DailySummary
 from collections import defaultdict
 import gviz_api
 
+QUERY_HOURLY_AVERAGE = """
+    SELECT
+        STRFTIME('%Y-%m-%dT%H:00:00.000', time) AS time,
+        AVG(temperature) AS temperature
+    FROM dashboard_measurement
+    WHERE time > '{:%Y-%m-%d %H:%M}'
+    GROUP BY STRFTIME('%Y-%m-%dT%H:00:00.000',time)"""
+
 def index(request):
     context = {
         'latest': Measurement.objects.latest()
@@ -13,15 +21,13 @@ def index(request):
     return render(request, 'dashboard/index.html', context)
     
 def data(request):
-    now = datetime.now()
+    now = datetime.now(None)
+    seven_days_ago = now - timedelta(7)
+    one_month_ago = now - timedelta(28)
     last_day = Measurement.objects.filter(time__range=[now - timedelta(1), now])
-    last_week = Measurement.objects.filter(time__range=[now - timedelta(7), now])
-    last_week_averages = Measurement.objects.raw("""
-        SELECT
-            STRFTIME('%Y-%m-%dT%H:00:00.000', time) AS time,
-            AVG(temperature) AS temperature
-        FROM dashboard_measurement
-        GROUP BY STRFTIME('%Y-%m-%dT%H:00:00.000',time)""")
+    last_week = Measurement.objects.filter(time__range=[seven_days_ago, now])
+    last_week_averages = Measurement.objects.raw(QUERY_HOURLY_AVERAGE.format(seven_days_ago))
+    last_month_averages = Measurement.objects.raw(QUERY_HOURLY_AVERAGE.format(one_month_ago))
     
     daily_data = gviz_api.DataTable(
         [("time", "datetime"), ("temperature", "number")],
@@ -33,6 +39,11 @@ def data(request):
         ((measurement.time, float(measurement.temperature)) for measurement in last_week_averages)
     )
 
+    monthly_data = gviz_api.DataTable(
+        [("time", "datetime"), ("temperature", "number")],
+        ((measurement.time, float(measurement.temperature)) for measurement in last_month_averages)
+    )
+    
     all_summary = DailySummary.objects.raw('SELECT DATE(time) AS day, MIN(temperature) AS minimum, MAX(temperature) AS maximum FROM dashboard_measurement GROUP BY DATE(time)')
 
     all_data = gviz_api.DataTable(
@@ -48,6 +59,7 @@ def data(request):
         'weekly_max': last_week.latest('temperature'),
         'daily_data': daily_data.ToJSCode("daily_data", columns_order=("time", "temperature"), order_by="time"),
         'weekly_data': weekly_data.ToJSCode("weekly_data", columns_order=("time", "temperature"), order_by="time"),
+        'monthly_data': monthly_data.ToJSCode("monthly_data", columns_order=("time", "temperature"), order_by="time"),
         'all_data': all_data.ToJSCode("all_data", columns_order=("day", "min", "min_again", "max", "max_again"), order_by="day")
     }
         
